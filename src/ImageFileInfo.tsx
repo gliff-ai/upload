93f8521cb748906bfe7680d25219c529f1d55a45
philipjackson <philiptgjackson@gmail.com>
2021-05-13 13:09:35 +0100

    Multiple (#17)
    * feat: enabled multiple upload, made ImageFileInfo attributes readonly
    
    * style: prettier-ignore in two places
    
    * fix: removed a space to satisfy prettier
    
    * feat: added num_slices to ImageFileInfo
    
    * feat: added num_channels to ImageFileInfo
    
    * fix: compute resolution_z the way the comment (from the old codebase) says it should is
    
    * style: prettier

--- src/ImageFileInfo.tsx
+++ src/ImageFileInfo.tsx
@@ -1,27 +1,45 @@
 import { v4 as guidGenerator } from "uuid";
 
+interface FileInfo {
+  fileName: string;
+  fileID?: string;
+  resolution_x?: number;
+  resolution_y?: number;
+  resolution_z?: number;
+  width: number;
+  height: number;
+  num_slices: number;
+  num_channels: number;
+}
+
 export class ImageFileInfo {
-  private fileName: string; // Name of imported image file
+  readonly fileName: string;
 
-  private fileID: string; // ID of image file
+  readonly fileID: string;
 
-  public resolution_x: number;
+  readonly resolution_x: number;
 
-  public resolution_y: number;
+  readonly resolution_y: number;
 
-  public resolution_z: number;
+  readonly resolution_z: number;
 
-  public width: number; // grid width boxes
+  readonly width: number;
 
-  public height: number; // grid height boxes
+  readonly height: number;
 
-  constructor(fileName: string, fileID?: string) {
-    this.fileName = fileName;
-    this.fileID = fileID || guidGenerator();
-    this.resolution_x = 0;
-    this.resolution_y = 0;
-    this.resolution_z = 0;
-    this.width = 0;
-    this.height = 0;
+  readonly num_slices: number;
+
+  readonly num_channels: number;
+
+  constructor(fileInfo: FileInfo) {
+    this.fileName = fileInfo.fileName;
+    this.fileID = fileInfo.fileID || guidGenerator();
+    this.resolution_x = fileInfo.resolution_x;
+    this.resolution_y = fileInfo.resolution_y;
+    this.resolution_z = fileInfo.resolution_z;
+    this.width = fileInfo.width;
+    this.height = fileInfo.height;
+    this.num_slices = fileInfo.num_slices;
+    this.num_channels = fileInfo.num_channels;
   }
 }
